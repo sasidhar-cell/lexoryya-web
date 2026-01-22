beb9bc977c52fd91c36be97267a90da645c16626
sasidhar-cell <sasidhar.joga23@gmail.com>
2026-01-22 20:29:07 +0530

    Add pricing page

---
+++ pages/pricing.js
@@ -0,0 +1,10 @@
+import Layout from "../components/Layout";
+
+export default function Pricing() {
+  return (
+    <Layout>
+      <h1>Pricing</h1>
+      <p>Access knowledge with flexible pricing options.</p>
+    </Layout>
+  );
+}
