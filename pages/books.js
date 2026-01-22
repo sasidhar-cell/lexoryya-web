af040414ce2f2f9945f5463105af477431e682bf
sasidhar-cell <sasidhar.joga23@gmail.com>
2026-01-22 20:27:15 +0530

    Add books page

---
+++ pages/books.js
@@ -0,0 +1,10 @@
+import Layout from "../components/Layout";
+
+export default function Books() {
+  return (
+    <Layout>
+      <h1>Books</h1>
+      <p>Browse textbooks by category.</p>
+    </Layout>
+  );
+}
