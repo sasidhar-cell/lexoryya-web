f8752879acf89e8b16d9f3b6ce86841166066994
sasidhar-cell <sasidhar.joga23@gmail.com>
2026-01-22 20:28:13 +0530

    Add categories page

---
+++ pages/categories.js
@@ -0,0 +1,10 @@
+import Layout from "../components/Layout";
+
+export default function Categories() {
+  return (
+    <Layout>
+      <h1>Categories</h1>
+      <p>Pharmacy, MBBS, Engineering, and more.</p>
+    </Layout>
+  );
+}
