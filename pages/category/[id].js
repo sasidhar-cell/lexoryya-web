66e424530e72b03ad2268783017207824dea2225
sasidhar-cell <sasidhar.joga23@gmail.com>
2026-01-24 16:51:45 +0530

    Add category dynamic page placeholder

---
+++ pages/category/[id].js
@@ -0,0 +1,10 @@
+import Layout from "../../components/Layout";
+
+export default function CategoryPage() {
+  return (
+    <Layout>
+      <h1>Category Page</h1>
+      <p>This is a placeholder. Data will load here.</p>
+    </Layout>
+  );
+}
