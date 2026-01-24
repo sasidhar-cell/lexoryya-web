f265d66c5f7f235b945729bb6995a601b1cac915
sasidhar-cell <sasidhar.joga23@gmail.com>
2026-01-24 15:59:33 +0530

    Update books.js

--- pages/books.js
+++ pages/books.js
@@ -8,7 +8,7 @@ export default function Books() {
       <ul>
         <li>
           <a
-            href="https://ermjlzaghkkqjyomhuny.supabase.co/storage/v1/object/sign/books/cancer-1.pdf?token=eyJraWQiOiJzdG9yYWdlLXVybC1zaWduaW5nLWtleV9hNDY5MWJlOS0xN2I5LTRmNWMtYWY5Ni1hZTE2ODc0NDIzNmIiLCJhbGciOiJIUzI1NiJ9.eyJ1cmwiOiJib29rcy9jYW5jZXItMS5wZGYiLCJpYXQiOjE3NjkyNDk2NDQsImV4cCI6MTgwMDc4NTY0NH0.0awPPF5Xi9HtyTpU4uJVnXa7rek_j6QzoMUVZhFkFv4"
+            href="https://ermjlzaghkkqjyomhuny.supabase.co/storage/v1/object/public/books/cancer-1.pdf"
             target="_blank"
             rel="noopener noreferrer"
           >
